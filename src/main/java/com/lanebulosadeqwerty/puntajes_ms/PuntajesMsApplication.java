68785d9554b342a953e42f3af483eb577bd11252
Sebastian Puerto <sebastian.cohnan@gmail.com>
2021-11-30 23:47:25 -0500

    Inicializado Backend Microservicio de Gestion de Puntajes

---
+++ src/main/java/com/lanebulosadeqwerty/puntajes_ms/PuntajesMsApplication.java
@@ -0,0 +1,23 @@
+package com.lanebulosadeqwerty.puntajes_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PuntajesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntajesMsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String darMensajeBienvenida() {
+		return """
+		<h1>La Nebulosa de Qwerty - Microservicio de Niveles</h1> <br/>
+		Hola! Estás en el backend del Microservicio de Puntajes de la aplicación La Nebulosa de Qwerty.
+		""";
+	}
+}
---
+++ src/test/java/com/lanebulosadeqwerty/puntajes_ms/PuntajesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanebulosadeqwerty.puntajes_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntajesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
